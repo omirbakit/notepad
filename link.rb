0d2862df9d9a67dc5660d6b1e91377b8a60419ee
omirbakit <omersat@mail.ru>
2021-02-01 21:08:10 +0500

    Notepad v.1, working )

--- link.rb
+++ link.rb
@@ -7,8 +7,16 @@ class Link < Post
   end
 
   def read_from_console
+    puts "Введите адрес ссылки"
+    @url = STDIN.gets.chomp
+
+    puts "Напишите пару слов о том, куда ведёт ссылка"
+    @text = STDIN.gets.chomp
   end
 
   def to_strings
+    time_string = "Создано: #{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+
+    return [@url, @text, time_string]
   end
 end
